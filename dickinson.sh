2f38319e8abbf1624911bf70fe1b10c014103011
Daniel W. Steinbrook <steinbro@post.harvard.edu>
2013-02-23 12:43:14 -0500

    better data cleaning

--- dickinson.sh
+++ dickinson.sh
@@ -6,8 +6,11 @@ then
   wget ftp://sailor.gutenberg.lib.md.us/gutenberg/1/2/2/4/12242/12242-8.txt
 fi
 
-# remove header/footer; hard-code content into HTML page
+# remove header/footer blocks; remove poem titles/Roman numerals; hard-code 
+# content into HTML page
 cat <(echo '<script type="text/plain" id="anthology">') \
-    <(tail -n +200 12242-8.txt | tail -r | tail -n +400 | tail -r) \
+    <(cat 12242-8.txt | \
+      sed '1,215d;2810,2965d;6960,7020d;10336,10739d' | \
+      sed '/^[^a-z]*[\.\!]/d' ) \
     <(echo '</script>') \
     dickinson.html.part > dickinson.html
